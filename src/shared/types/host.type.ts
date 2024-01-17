c05eab630474fef437f1bba8d2b51ecf7b100e7b
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2024-01-17 23:52:06 +0200

    Adds types for imported entities.

---
+++ src/shared/types/host.type.ts
@@ -0,0 +1,7 @@
+export type Host = {
+  name: string;
+  email: string;
+  avatarUrl: string;
+  password: string;
+  isPro: boolean;
+}
---
+++ src/shared/types/offer.type.ts
@@ -0,0 +1,25 @@
+import { Host } from './host.type.js';
+import { Location } from './location.type.js';
+import { CityName } from './city-name.enum.js';
+import { HousingType } from './housing-type.enum.js';
+import { Good } from './good.enum.js';
+
+export type Offer = {
+  title: string;
+  description: string;
+  date: Date;
+  city: CityName;
+  previewImage: string;
+  images: string[] | string;
+  isPremium: boolean;
+  isFavorite: boolean;
+  rating: number;
+  housingType: typeof HousingType;
+  bedrooms: number;
+  maxAdults: number;
+  price: number;
+  goods: Good[];
+  host: Host;
+  comments: number;
+  location: Location;
+}
