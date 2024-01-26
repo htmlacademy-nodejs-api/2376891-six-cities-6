b5491bf9e4fa13b993588a028277dda5bb1683c4
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2024-01-26 14:03:04 +0200

    Adds streams and refactors the TSVFileReader class.

---
+++ src/shared/helpers/offer.ts
@@ -0,0 +1,42 @@
+import { TOffer, HousingType, CityName, Good, HostAccountType, IHost } from '../types/index.js';
+import { toBoolean } from '../../utils/common.js';
+
+export function createOffer(offerData: string): TOffer {
+  const [
+    title, description, date, city, previewImage, images, isPremium, isFavorite, rating, housingType,
+    bedrooms, maxAdults, price, goods, name, email, avatarUrl, password, accountType, comments, latitude, longitude
+  ] = offerData.replace('\n', '').split('\t');
+
+  const host: IHost = {
+    name,
+    email,
+    avatarUrl,
+    password,
+    accountType: accountType as HostAccountType
+  };
+
+  const location = {
+    latitude: Number(latitude),
+    longitude: Number(longitude)
+  };
+
+  return {
+    title,
+    description,
+    date: new Date(date),
+    city: city as CityName,
+    previewImage,
+    images: images.split(';'),
+    isPremium: toBoolean(isPremium),
+    isFavorite: toBoolean(isFavorite),
+    rating: Number.parseFloat(rating),
+    housingType: housingType as HousingType,
+    bedrooms: Number.parseInt(bedrooms, 10),
+    maxAdults: Number.parseInt(maxAdults, 10),
+    price: Number.parseInt(price, 10),
+    goods: goods.split(';') as Good[],
+    host,
+    comments: Number.parseInt(comments, 10),
+    location
+  };
+}
