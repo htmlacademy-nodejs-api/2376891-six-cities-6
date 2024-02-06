7c75f529bd4008806d55c0653b657c869376f1b4
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2024-02-06 23:03:00 +0200

    Extends the UserDocument interface. Uses built-in validation.

--- src/shared/modules/user/user.model.ts
+++ src/shared/modules/user/user.model.ts
@@ -1,14 +1,36 @@
 import { Schema, Document, model } from 'mongoose';
-import { IUser } from '../../types/index.js';
+import { IUser, UserAccountType } from '../../types/index.js';
 
-export interface UserDocument extends IUser, Document {}
+export interface UserDocument extends IUser, Document {
+  createdAt: Date,
+  updatedAt: Date,
+}
 
 const userSchema = new Schema({
-  name: String,
-  email: String,
+  name: {
+    type: String,
+    require: true,
+    minlength: [1, 'Min length for name is 1'],
+    maxlength: [15, 'Max length for name is 15'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    match: [/^([\w-\\.]+@([\w-]+\.)+[\w-]{2,4})?$/, 'Email is incorrect'],
+    require: true,
+  },
   avatarUrl: String,
-  password: String,
-  accountType: String,
-});
+  password: {
+    type: String,
+    require: true,
+    minlength: [6, 'Min length for password is 6'],
+    maxlength: [12, 'Max length for password is 12'],
+  },
+  accountType: {
+    type: String,
+    require: true,
+    enum: [`${UserAccountType.Common}`, `${UserAccountType.Pro}`],
+  },
+}, {timestamps: true});
 
 export const UserModel = model<UserDocument>('User', userSchema);
