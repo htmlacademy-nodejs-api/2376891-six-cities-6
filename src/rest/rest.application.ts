d3fff56522afe6f750df65dcac9630038ba85865
Snezhana Sterkhova <snezhana.sterkhova2015@gmail.com>
2024-02-25 21:53:55 +0200

    Adds a UserController controller and error handling.

--- src/rest/rest.application.ts
+++ src/rest/rest.application.ts
@@ -5,6 +5,7 @@ import { Config, RestSchema } from '../shared/libs/config/index.js';
 import { EComponent } from '../shared/types/index.js';
 import { DatabaseClient } from '../shared/libs/database-client/index.js';
 import { getMongoURI } from '../shared/helpers/index.js';
+import { Controller, ExceptionFilter } from '../shared/libs/rest/index.js';
 
 @injectable()
 export class RestApplication {
@@ -14,6 +15,8 @@ export class RestApplication {
     @inject(EComponent.Logger) private readonly logger: Logger,
     @inject(EComponent.Config) private readonly config: Config<RestSchema>,
     @inject(EComponent.DatabaseClient) private readonly databaseClient: DatabaseClient,
+    @inject(EComponent.ExceptionFilter) private readonly appExceptionFilter: ExceptionFilter,
+    @inject(EComponent.UserController) private readonly userController: Controller,
   ) {
     this.server = express();
   }
@@ -35,6 +38,18 @@ export class RestApplication {
     this.server.listen(port);
   }
 
+  private async _initControllers() {
+    this.server.use('/users', this.userController.router);
+  }
+
+  private async _initMiddleware() {
+    this.server.use(express.json());
+  }
+
+  private async _initExceptionFilters() {
+    this.server.use(this.appExceptionFilter.catch.bind(this.appExceptionFilter));
+  }
+
   public async init() {
     this.logger.info('Application initialization');
     this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
@@ -42,6 +57,18 @@ export class RestApplication {
     this.logger.info('Init database...');
     await this.initDb();
     this.logger.info('Init database completed');
+
+    this.logger.info('Init app-level middleware');
+    await this._initMiddleware();
+    this.logger.info('App-level middleware initialization completed');
+
+    this.logger.info('Init controllers');
+    await this._initControllers();
+    this.logger.info('Controller initialization completed');
+
+    this.logger.info('Init exception filters');
+    await this._initExceptionFilters();
+    this.logger.info('Exception filters initialization completed');
 
     this.logger.info('Try to init server...');
     await this._initServer();
